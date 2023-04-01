f255a2f6ac6b035e691859a69abb3dc5835b64f5
Augusto Dias <gutodiasux@gmail.com>
2023-04-01 10:58:33 -0300

    feat: add CreateParnerInputs model

---
+++ src/domain/models/create-partner-inputs.ts
@@ -0,0 +1,10 @@
+export type CreatePartnerInputs = {
+  name: string
+  resposible_name: string
+  responsible_email: string
+  responsible_phone: string
+  street: string
+  number: string
+  phone: string
+  zipcode: string
+}
--- src/domain/models/index.ts
+++ src/domain/models/index.ts
@@ -1,3 +1,4 @@
+export * from './create-partner-inputs'
 export * from './login-input'
 export * from './login-output'
 export * from './patients-response'
